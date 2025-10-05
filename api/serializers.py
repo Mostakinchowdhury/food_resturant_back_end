206d51209a2254e9e016cc5faf599ff2e39ba832
Mostakin chowdhury <chowdhurymostakin02@gmail.com>
2025-10-05 16:02:30 +0600

    http to https

--- api/serializers.py
+++ api/serializers.py
@@ -128,11 +128,11 @@ class CategorySerializer(serializers.ModelSerializer):
         model = Category
         fields = ["id", "name", "description","image"]
         read_only_fields = ['id',]
-        def get_image(self,obj):
-         request=self.context.get("request")
-         if obj.file and hasattr(obj.file, 'url'):
-            return request.build_absolute_uri(obj.file.url)
-         return None
+    def get_image(self,obj):
+      request=self.context.get("request")
+      if obj.image and hasattr(obj.image, 'url'):
+            return request.build_absolute_uri(obj.image.url)
+      return None
 
 # Supercategory serializer
 
